3954725cdc1eafe4ffe6067a0c85c1271261cd8c
RvPA <23041589+RvPA-Roopa@users.noreply.github.com>
2026-06-03 19:00:40 -0500

    removed option for now

--- wl3/options.py
+++ wl3/options.py
@@ -282,15 +282,6 @@ class BigCoinsanity(Toggle):
     default = 0
 
 
-class Enemizer(Toggle):
-    """Randomize enemy placement across levels. Walkable enemies (Snake,
-    Webber, Futamogu), stepping platforms, gates, and bosses stay at
-    their vanilla positions; ~1781 rooms get fresh enemy bundles.
-    Rooms with throw-only blocks are guaranteed a throwable enemy."""
-    display_name = "Enemizer"
-    default = 0
-
-
 class HiddenPassagesRevealed(Toggle):
     """Hidden passages are revealed with their corresponding cracked/non-cracked blocks
     Currently, hidden throw/fire/yarn blocks aren't included in this"""
@@ -374,7 +365,6 @@ class WL3Options(PerGameCommonOptions):
     reduce_flashing:              ReduceFlashing
     non_stop_chests:              NonStopChests
     bigcoinsanity:                BigCoinsanity
-    enemizer:                     Enemizer
     hidden_passages_revealed:     HiddenPassagesRevealed
     trap_fill:                    TrapFill
     death_link:                   DeathLink
